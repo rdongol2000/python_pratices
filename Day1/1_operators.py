db34f1db8a3a47d491717260f8986b7716a52fa7
rdongl2000 <ram.dongol.maharjan@gmail.com>
2024-09-20 18:43:41 +0545

    First Code

---
+++ Day1/1_operators.py
@@ -0,0 +1,59 @@
+# Operator are the special symbols which are used or carry out arithmetical and logical operations.
+# Like in any other language python also has it's set of operators
+
+# Arithmetic Operators
+# Logical Operators
+# Relational Operators
+# Assignment Operators
+# Membership Operators 
+# Identity Operators
+
+
+# Arithmetic Operators (+, -, /, *, **, %)
+a = 1 # Python statement
+b = 2 # Python statement
+
+result = a+b 
+print ("The result of the sum is", result)
+
+result = b ** 2
+print(result) # 4
+
+result = 5 % 2
+print (result) # 1
+
+result = 5 / 2
+print(result) # 2.5
+
+
+# Relational / Comparison Opertors (>, <, >=, <=, ==,!=)
+# The result of comparison operations are either True or False
+
+a = 20
+b = 15 
+
+print (a > b) # True
+print (a < b) # False
+print (a == b) # False
+print ( a >= b) # True
+print (a <= b) # False
+print (a !=b) # True
+
+
+# Logical Operators (and, or, not)
+# The result of logical operations are either True or False
+
+print (True and True) # True
+print (True and False) # False
+print (False and True) # False
+print (False and False) # False 
+
+print(True or True) # True
+print (True or False) # True
+print(False or True) # True
+print(False or False) # False
+# 2nd part
+print (not True) # False
+print (not False) # True
+
+
