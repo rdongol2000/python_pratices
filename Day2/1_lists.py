a0e3fe5eb3d6c59526286185a5c6402484275ac8
rdongl2000 <ram.dongol.maharjan@gmail.com>
2024-09-29 11:59:16 +0545

    29-9-2024

--- Day2/1_lists.py
+++ Day2/1_lists.py
@@ -47,5 +47,28 @@ print(data[2:9:2]) # ["b", "d", "f", "h"] ['c', 'e', 'g', 'i']
 print(data[3:9]) # ["d", "e", "f", "g", "h", "i"]
 print(data[8:2]) # []
 print(data[5:]) # ["f", "g", "h", "i", "j"]
-print(data[:4] # ["a", "b", "c", "d"]
-print(data[-9:-7]) # []
+print(data[:4]) # ["a", "b", "c", "d"]
+print(data[-9:-3]) # ["b", "c", "d", "e", "f", "g"]
+print(data[-8:8]) # ["c", "d", "e", "f", "g" "h"]
+print(data[9:-7]) # []
+print(data[-4:-9]) # []
+
+# List Opertions 
+
+# Concatenation (+)
+
+ a = [1, 2, 3]
+ b = [4, 5, 6]
+ result = a + b
+ print(result) # [1, 2, 3, 4, 5, 6]
+
+
+
+# Repetition / broadcast Operation (*)
+a = [1, 2]
+print(a * 3) # [1,2,1,2,2,1,2]
+
+
+# Membership Operation ("in" and "not in")
+print ("a" in ["a", "e", "i"]) # True
+print("e" not in ["a", "e", "i"]) # False
